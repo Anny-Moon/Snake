14fe85ab61e102e411a0ddbeacdd0631410282f3
Sinelnikova <anna.sinelnikova@physics.uu.se>
2018-05-01 14:29:32 +0200

    clening up

--- source/main.cpp
+++ source/main.cpp
@@ -37,43 +37,8 @@
 #include <time.h>
 #include <stdlib.h>
 
-#include <string> // required for std::string
-#include <sys/types.h> // required for stat.h
-#include <sys/stat.h>
-
-//#include "../include/Global.h"
 int main()
 {	
-/*    //create or chech if directory exist
-    bool isDirExists=false;
-    int nError = 0;
-    mode_t nMode = 0733; // UNIX style permissions
-    
-    struct stat st;
-    if(stat(PATH_TO_DIR,&st) == 0){
-	if(st.st_mode & S_IFDIR != 0){
-//	    printf("exist\n");
-	    isDirExists = true;
-	}
-    }
-    else{
-//	printf("NOT exist\n");
-	isDirExists = false;
-    }
-    
-    if(!isDirExists){
-	printf("I created the directory: '%s'.\n", PATH_TO_DIR);
-	printf("It will be used for the game files.\n", PATH_TO_DIR);
-	nError = mkdir(PATH_TO_DIR, nMode);
-    }
-    
-    if (nError != 0) {
-	printf("Error:\n\tcannot create dir '%s'.\n",PATH_TO_DIR);
-	exit(1);
-    }
-
-    ////////////////////////////////////
-*/    
     Util::createDir(PATH_TO_DIR_STR);
     if(Util::isFirstRun){
 	for(int i=0;i<Util::levelDefault.size();i++)
@@ -177,25 +142,10 @@ int main()
 	    default:
 		Level::zero(ch);
 		break;
-	//if(ch == '1')
-	//    Level::one(ch);
-	
-	//else if(ch == '2')
-	//    Level::two(ch);
-	
-	//else if(ch == '3')
-	//    Level::three(ch);
-	
-	//else if(ch == 'Q' || ch == 'q')
-	//    break;
-	
-	//else
-	//    Level::zero(ch);
 	}
     }
     // Clear ncurses data structures (don't remove thus!)
     endwin();
-//    printf("It was a good game!\n");
     printf("\t\tThank you for the game!\n");
     return 0;
 }
